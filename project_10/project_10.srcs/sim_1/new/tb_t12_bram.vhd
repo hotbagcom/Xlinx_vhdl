14c23e10f0c1796058e04af278b93d9e6a28363e
Arif Yardım <96440900+hotbagcom@users.noreply.github.com>
2024-08-06 13:19:44 +0300

    ram_usedge_study

---
+++ project_10/project_10.srcs/sim_1/new/tb_t12_bram.vhd
@@ -0,0 +1,146 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06.08.2024 08:56:37
+-- Design Name: 
+-- Module Name: tb_t12_bram - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+--use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.log2_pck.all;
+
+entity tb_t12_bram is
+generic (
+RAM_WIDTH 		: integer 	:= 32;				-- Specify RAM data width
+RAM_DEPTH 		: integer 	:= 64;				-- Specify RAM depth (number of entries)
+RAM_PERFORMANCE : string 	:= "LOW_LATENCY"    -- Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
+-- RAM_PERFORMANCE : string 	:= "HIGH_PERFORMANCE"    -- Select "HIGH_PERFORMANCE" or "LOW_LATENCY"
+);
+end tb_t12_bram;
+
+architecture Behavioral of tb_t12_bram is
+
+component t12_bram is
+generic (
+RAM_WIDTH 		: integer 	:= 32;				-- Specify RAM data width
+RAM_DEPTH 		: integer 	:= 64;				-- Specify RAM depth (number of entries)
+RAM_PERFORMANCE : string 	:= "LOW_LATENCY"    -- Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
+);
+port (
+addr : in std_logic_vector(log2_c(RAM_DEPTH)-1 downto 0);    -- Address bus, width determined from RAM_DEPTH
+din  : in std_logic_vector(RAM_WIDTH-1 downto 0);		  		-- RAM input data
+clk  : in std_logic;                       			  		-- Clock
+we   : in std_logic;                       			  		-- Write enable
+dout : out std_logic_vector(RAM_WIDTH-1 downto 0)   			-- RAM output data
+);
+end component;
+
+signal addra : std_logic_vector((log2_c(RAM_DEPTH)-1) downto 0)	:= (others => '0');
+signal dina  : std_logic_vector(RAM_WIDTH-1 downto 0) 			:= (others => '0');
+signal clka  : std_logic 										:= '0';            
+signal wea   : std_logic 										:= '0';                      
+signal douta_1 : std_logic_vector(RAM_WIDTH-1 downto 0);
+
+constant c_clkperiod	: time := 10 ns;
+
+begin
+
+DUT : t12_bram
+generic map(
+RAM_WIDTH 		=> 	RAM_WIDTH 		   ,
+RAM_DEPTH 		=> 	RAM_DEPTH 		   ,
+RAM_PERFORMANCE =>  RAM_PERFORMANCE    
+)
+port map(
+addr => addra  ,
+din  => dina   ,
+clk  => clka   ,
+we   => wea    ,
+dout => douta_1
+);
+
+P_CLKGEN : process begin
+
+clka	<= '1';
+wait for c_clkperiod/2;
+clka	<= '0';
+wait for c_clkperiod/2;
+
+end process P_CLKGEN;
+
+
+D_loop : process begin 
+constant  adress_0 : std_logic_vector( (log2_c(RAM_DEPTH)-1) downto 0 ) := '000000' ;
+
+
+end process D_loop ;
+
+
+
+P_STIMULI : process begin
+
+
+
+
+wea		<= '1';
+addra 	<= "000000";
+dina	<= x"F0F0F0F0";
+wait for c_clkperiod;
+addra 	<= "000001";
+dina	<= x"1234FFFF";
+wait for c_clkperiod;
+addra 	<= "000010";
+dina	<= x"ABCD0000";
+wait for c_clkperiod;
+addra 	<= "000011";
+dina	<= x"00111100";
+
+
+
+wait for c_clkperiod/2;
+wea		<= '0';
+
+wait for c_clkperiod/2;
+addra 	<= "000000";
+wait for c_clkperiod;
+addra 	<= "000001";
+wait for c_clkperiod;
+addra 	<= "000010";
+wait for c_clkperiod;
+addra 	<= "000011";
+wait for c_clkperiod;
+
+assert false ;
+report "SIM DONE";
+--severity failure;
+
+end process P_STIMULI;
+
+end Behavioral;
--- project_10/project_10.srcs/sources_1/new/t12_bram.vhd
+++ project_10/project_10.srcs/sources_1/new/t12_bram.vhd
@@ -1,151 +1,72 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05.08.2024 13:18:06
--- Design Name: 
--- Module Name: t12_bram - t12_bram_bhvrl
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
---Insert the following in the architecture before the begin keyword
---  The following function calculates the address width based on specified RAM depth
-
-package ram_pkg is
-function clogb2( depth : natural) return integer ;
-end ram_pkg;
+ package log2_pck is
+ function log2_c(depth :in natural ) return integer;
+ end log2_pck ;
 
 
-
-
-
-
-package body ram_pkg is
-function clogb2( depth : natural) return integer is
-variable temp    : integer := depth;
-variable ret_val : integer := 0;
-begin
+package body log2_pck is
+ function log2_c(depth :in natural ) return integer is
+ variable temp    : integer := depth;
+ variable ret_val : integer := 0;
+ begin
     while temp > 1 loop
         ret_val := ret_val + 1;
         temp    := temp / 2;
     end loop;
-    return ret_val;
-end function;
-
-end package body ram_pkg;
-
+  	return ret_val;
+ end function;
+ end package body log2_pck ;
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.Numeric_STD.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use std.textio.all;
-use work.ram_pkg.ALL ;
+use work.log2_pck.all;
+
+
 
 entity t12_bram is
-    Generic(
-        constant RAM_WIDTH : integer := 4;            		-- Specify RAM data width
-        constant RAM_DEPTH : integer := 8; 				        -- Specify RAM depth (number of entries)
-        constant RAM_PERFORMANCE : string := "HIGH_PERFORMANCE" ; 		-- Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
-        --constant C_RAM_PERFORMANCE : string := "LOW_LATENCY" ; 		-- Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
-        
-        constant C_INIT_FILE : string := "read_only_mem.mem"    				 -- Specify name/location of RAM initialization file if using one (leave blank if not)
-        
-    );
-    Port (
-        W_adrs_in  : in std_logic_vector((clogb2(RAM_DEPTH)-1) downto 0);    -- Write address bus, width determined from RAM_DEPTH
-        R_adrs_in  : in std_logic_vector((clogb2(RAM_DEPTH)-1) downto 0);    -- Read address bus, width determined from RAM_DEPTH
-        Din_in     : in std_logic_vector(RAM_WIDTH-1 downto 0);		        -- RAM input data
-        clk_io     :inout std_logic;                       			        -- Clock
-        W_enable   :out std_logic ;                      			           -- Write enable
-        M_enable   :out std_logic;                       			           -- RAM Enable, for additional power savings, disable port when not in use
-        Dout_out   :out std_logic_vector(RAM_WIDTH-1 downto 0) 		        -- RAM output data
+generic(
+RAM_WIDTH 		: integer 	;				-- Specify RAM data width
+RAM_DEPTH 		: integer 	;				-- Specify RAM depth (number of entries)
+RAM_PERFORMANCE : string 	    -- Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
 
-    );
+);
+port(
+clk : in std_logic;
+we : in std_logic;
+addr : in std_logic_vector(log2_c(RAM_DEPTH)-1 downto 0);
+din : in std_logic_vector(RAM_WIDTH-1 downto 0);
+dout : out std_logic_vector(RAM_WIDTH-1 downto 0)
+);
 end t12_bram;
 
-architecture t12_bram_bhvrl of t12_bram is
+architecture syn of t12_bram is
+type RamType is array (0 to RAM_DEPTH-1) of bit_vector(RAM_WIDTH-1 downto 0);
+impure function InitRamFromFile( RamFileName: in string) return RamType is
+FILE RamFile : text is in RamFileName;
+variable RamFileLine : line;
+variable RAM : RamType;
 
-    constant C_RAM_WIDTH : integer := RAM_WIDTH;            		-- Specify RAM data width
-    constant C_RAM_DEPTH : integer := RAM_DEPTH ; 				     -- Specify RAM depth (number of entries)
-    constant C_RAM_PERFORMANCE : string := RAM_PERFORMANCE ; 		-- Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
-    constant c_clock : integer := 1_000_000_000 ;
-    constant c_clockPeriod : time := (1000 ms / c_clock)  ;         -- clock periode is 1 ns 
-
-   signal  clk : std_logic := '1' ;
-
-   
-        -- RAM signal declaration
-  type ram_type is array (0 to C_RAM_DEPTH-1) of std_logic_vector(C_RAM_WIDTH-1 downto 0);
-  signal ram_data : ram_type := (others => (others => '0'));
-
-  -- Register for output data
-  signal Dout_reg : std_logic_vector(C_RAM_WIDTH-1 downto 0) := (others => '0');
-
-  -- File handling variables
- shared variable ram_index : integer := 0;
- shared variable direction : std_logic := '1';
- 
- 
 begin
-  -- Initialize RAM from file
-  read_file : process
-  file ramfile    : text is in C_INIT_FILE;
-  variable ramfileline : line ;
-  variable bitvec : bit_vector((C_RAM_DEPTH*C_RAM_WIDTH)-1 downto 0)  ;
---  variable slv_vec : std_logic_vector(C_RAM_WIDTH-1 downto 0);
-  begin
-      for i in ram_type'range loop
-        readline(ramfile, ramfileline); 
-        read(ramfileline,  bitvec );
-        
-        ram_data(i) <= to_stdlogicvector(bitvec) ;
-      end loop;
-  end process read_file;
+for I in RamType'range loop
+readline(RamFile, RamFileLine);
+read(RamFileLine, RAM(I));
+end loop;
+return RAM;
+end function;
+
+signal RAM : RamType := InitRamFromFile("read_only_mem.mem");
+begin
 
 
-
-
---begin--
-
-    clk <= not clk after c_clockPeriod /2 ;
-    clk_io <= clk ;   
-    process (clk) begin
+    process(clk)
+    begin
         if (rising_edge(clk)) then
-           if(direction='1') then 
-           ram_index := ram_index +1;
-            Dout_out <= ram_data( 15 downto 0 ) ;
-            --( ((ram_index  + 1)*C_RAM_WIDTH) -1 )  downto ram_index*C_RAM_WIDTH 
-           else if ()then 
-           
-           ram_index := ram_index -1;
-           
-           end if ;
-        end if;
-        
-        if (falling_edge(clk)) then
-           if(ram_index = (C_RAM_DEPTH-1) ) then
-            direction := '0';
-           else if( ram_index = 0 ) then
-            direction := '1';
-           end if ;
+            if we = '1' then
+                RAM(to_integer(unsigned(addr))) <= to_bitvector(din);
+            end if;
+            dout <= to_stdlogicvector(RAM(to_integer(unsigned(addr))));
         end if;
     end process;
-    
-
-    
-end t12_bram_bhvrl;
-
-    
-
+end syn;
